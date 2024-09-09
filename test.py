e5a580f7031b52a1f3ae6c5a50a1caf35a825b15
Ethan Mac <home@Ethans-MacBook-Air.local>
2024-09-09 13:43:24 -0400

    test

--- test.py
+++ test.py
@@ -1,24 +1,20 @@
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-    print(arr)
-
-insertion_sort([12,11,15,10,9,1,5,6,13,8])
+import copy
 
 
-
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    print(arr)
+orig = [[1, 2, 3], [4, 5, 6]]
+not_copy = orig
+s_copy = orig.copy()
+d_copy = copy.deepcopy(orig)
 
 
-bubble_sort([12,11,15,10,9,1,5,6,13,8])
+orig[1][1] = 3
+not_copy[0][0] = 2
+s_copy[0][2] = 10
+d_copy[1][2] = 15
+
+print(orig)
+print(not_copy)
+print(s_copy)
+print(d_copy)
+
+
